27314c54b5b3df67a0db21279fb8318f1016a6e3
vzexi <vastcreation@protonmail.com>
2025-04-16 08:50:44 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration file for account status
+const config = {
+  // Set your account status here
+  // Options: "termed", "new", "limited"
+  accountStatus: "limited",
+
+  // Custom status text (optional)
+  // If not provided, default text will be used based on status
+  customStatusText: "",
+}
